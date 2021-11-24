5b1475affa17ac29e697a47b4b16e8237d873bbf
heitormagaldi <heitormagaldi@yahoo.com.br>
2021-11-23 22:49:33 -0300

    fix:it was tested all coverage

--- src/sum.test.js
+++ src/sum.test.js
@@ -1,5 +1,21 @@
-const operator = require("./sum");
+const operator = require("./operators");
 
 test("Sum 1 + 1 should be equal 2", () => {
   expect(operator.sum(1, 1)).toBe(2);
+});
+
+test("Sum 1 + 1 + 5 should be equal 7", () => {
+  expect(operator.sum5(1, 1)).toBe(7);
+});
+
+test("Sum 1 + 1 + 6 should be equal 8", () => {
+  expect(operator.sum6(1, 1)).toBe(8);
+});
+
+test("Times 2 * 5 should be equal 10", () => {
+  expect(operator.times(2, 5)).toBe(10);
+});
+
+test("Divided by 10 / 2 should be equal 5", () => {
+  expect(operator.dividedBy(10, 2)).toBe(5);
 });
